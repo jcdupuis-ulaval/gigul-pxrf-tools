298dbdbee0aa2c43d515e826a71d270c812c9909
J. Christian Dupuis <christian.dupuis@ggl.ulaval.ca>
2020-06-02 12:29:34 -0400

    Added pick_peak_heavy
    Added the function added_pick_peak_heavy contributed by Thomas

---
+++ proc/pick_Peak_heavy.py
@@ -0,0 +1,24 @@
+import numpy as np
+import gigul_pxrf_tools as gigul
+import os
+
+# File setup for data and results################################
+#fname ='TDCAEXD332727z_paire5'
+ddir = '../results/CSV/denoised/'
+rdir = '../results/peaks/heavy/'
+idir = '../results/PNG/heavy/'
+# Filter parameters #############################################
+amp_threshold=100.0
+slope_threshold =-10.0
+peak_half_width = 5
+#################################################################
+
+flist = os.listdir(path=ddir)
+    
+for fname in flist:
+    data=np.genfromtxt(ddir+fname,delimiter=',')
+    trace = data[1100:2048,1]
+    ch = data[1100:2048:,0] 
+    peak_est = gigul.estimate_peaks(trace,amp_threshold,slope_threshold)
+    peaks = gigul.refine_peaks(trace,peak_est,peak_half_width,ch,rdir+'picks_'+fname)
+    gigul.show_peaks(trace,ch,peaks,peak_est,idir+'picks_'+fname)
