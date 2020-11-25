72a09b58265bbae16ebc9c145c3b64d6a6325b89
Thomas-Deb <66323029+Thomas-Deb@users.noreply.github.com>
2020-11-25 18:59:50 -0500

    Update pick-peaks.py

--- proc/pick-peaks.py
+++ proc/pick-peaks.py
@@ -15,29 +15,41 @@ import os
 
 
 # File setup for data and results################################
-#fname ='TDCAEXD332727z_paire5'
+tdir = '../data/Terrain/'
 ddir = '../results/CSV/denoised/'
 rdir = '../results/peaks/'
-idir = '../results/PNG/'
-# Filter parameters #############################################
-amp_sensitivity=0.1    # Only values above mu + sigma*amp_threshold will be considered for peaks 
-slope_sensitivity=0.05  # Only values below mu - sigma threshold will be considered for peaks
-peak_half_width = 5     # Width of the data selection to fit polynomial function
-#################################################################
-
-flist = os.listdir(path=ddir)
-
-
-for fname in flist:
-    data=np.genfromtxt(ddir+fname,delimiter=',')
-    # We can try to scale the data between 0 and 1 to make picking easier 
-    # with the thresholds
-    trace = data[:,1]
-    trace_norm = gigul.scale_trace(trace)
-    ch = data[:,0] 
-    # We generate our peak estimates on the normalized data
-    peak_est = gigul.estimate_peaks(trace_norm,ch,amp_sensitivity,slope_sensitivity)
-    # We pick on the real data starting from our pick estimates
-    peaks = gigul.refine_peaks(trace,peak_est,peak_half_width,ch,rdir+'picks_'+fname)
-
-    gigul.show_peaks(trace,ch,peaks,peak_est,idir+'picks_'+fname)
+vdir = '../results/statistics/'
+idir = '../results/PNG/denoised/'
+fname_list = os.listdir(path=tdir)
+b = len(fname_list)
+a = 0
+for value in fname_list:
+    fname= fname_list[a]
+    fname=fname[:-4]
+    a = a+1
+    # Filter parameters #############################################
+    amp_sensitivity=0.1    # Only values above mu + sigma*amp_threshold will be considered for peaks 
+    slope_sensitivity=0.05  # Only values below mu - sigma threshold will be considered for peaks
+    peak_half_width = 5     # Width of the data selection to fit polynomial function
+    #################################################################
+    
+    flist = os.listdir(path=ddir)
+    flist = [i for i in flist if i.startswith(fname)]
+    
+    for fname in flist:
+        data=np.genfromtxt(ddir+fname,delimiter=',')
+        # We can try to scale the data between 0 and 1 to make picking easier 
+        # with the thresholds
+        trace = data[:,1]
+        trace_norm = gigul.scale_trace(trace)
+        ch = data[:,0] 
+        # We generate our peak estimates on the normalized data
+        peak_est = gigul.estimate_peaks(trace_norm,ch,amp_sensitivity,slope_sensitivity,fname)
+        # We pick on the real data starting from our pick estimates
+        peaks = gigul.refine_peaks(trace,peak_est,peak_half_width,ch,rdir+'picks_'+fname)
+    
+        gigul.show_peaks(trace,ch,peaks,peak_est,idir+'picks_'+fname)
+    if a < b:
+        continue
+    if a == b:
+        break
