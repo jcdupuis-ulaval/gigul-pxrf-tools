1e0cdc767b8838f5d02a761cc06990abeeaa96cd
Thomas-Deb <66323029+Thomas-Deb@users.noreply.github.com>
2020-11-25 19:00:57 -0500

    Update QA-QC-peaks.py

--- proc/QA-QC-peaks.py
+++ proc/QA-QC-peaks.py
@@ -9,25 +9,42 @@ import numpy as np
 import matplotlib.pyplot as plt
 import os
 
-# File setup for data ################################
+# File setup for data and results################################
+tdir = '../data/Terrain/'
 rdir = '../results/CSV/merged/'
 pdir = '../results/peaks/'
 cdir = '../results/CSV/denoised/'
-idir = '../results/PNG/'
-# File list of all our directories ######################################
-flist_raw = os.listdir(path=rdir) # Merged raw traces 
-flist_clean = os.listdir(path=cdir) # Merged traces without background
-flist_peaks = os.listdir(path=pdir) # Peaks that were identified 
-#########################################################################
-i=0  # counter required to keep track of the matching files in our loop
-
-for fname in flist_raw:
-    plt.figure()
-    raw_data=np.genfromtxt(rdir+fname,delimiter=',')
-    peaks = np.genfromtxt(pdir+flist_peaks[i],delimiter=',')
-    clean_data = np.genfromtxt(cdir+flist_clean[i],delimiter=',')
-    plt.plot(raw_data[:,0],raw_data[:,1],peaks[:,0],peaks[:,1],'+',clean_data[:,0],clean_data[:,1])
-    plt.grid()
-    plt.show()
-    i=i+1
-
+idir = '../results/PNG/QA_QC/'
+fname_list = os.listdir(path=tdir)
+b = len(fname_list)
+a = 0
+for value in fname_list:
+    fname= fname_list[a]
+    fname=fname[:-4]
+    a = a+1
+# Filter parameters #############################################
+    flist_raw = os.listdir(path=rdir)
+    flist_raw = [i for i in flist_raw if i.startswith(fname)]
+    flist_clean = os.listdir(path=cdir)
+    flist_clean = [i for i in flist_clean if i.startswith(fname)]
+    flist_peaks = os.listdir(path=pdir)
+    flist_peaks = [i for i in flist_peaks if i.startswith('picks_'+fname)]
+    i=0
+    for fname in flist_raw:
+        plt.figure()
+        raw_data=np.genfromtxt(rdir+fname,delimiter=',')
+        peaks = np.genfromtxt(pdir+flist_peaks[i],delimiter=',')
+        clean_data = np.genfromtxt(cdir+flist_clean[i],delimiter=',')
+        plt.plot(raw_data[:,0],raw_data[:,1],peaks[:,0],peaks[:,1],'+',clean_data[:,0],clean_data[:,1])
+        plt.grid()
+        plt.title(fname)
+        plt.legend(['original signal','peaks','normalised signal'])
+        plt.xlabel('Channels')
+        plt.ylabel('Total Count')
+        plt.savefig(idir+fname[:-3]+'.png', format='png')
+        plt.show()
+        i=i+1
+    if a < b:
+        continue
+    if a == b:
+        break
